6a62d92e6bc0d4c8055a8eefd0b065286bfe5459
Paul Mascall <pjwmascall@hotmail.com>
2022-03-09 11:32:52 +0000

    set up database

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const MongoClient = require('mongodb').MongoClient;
+
+const createRouter = require('./helpers/createRouter.js');
+
+MongoClient.connect('mongodb://127.0.0.1:27017', {useUnifiedTopology: true})
+    .then(client => {
+        const db = client.db('hotel');
+        const bookingsCollection = db.collections('bookings');
+        const bookingsRouter = createRouter(bookingsCollection);
+        app.use('/api/bookings', bookingsRouter);
+    })
+    .catch( 
+        console.error
+    );
+
+app.listen(5000, function() {
+    console.log(`Listening on port ${this.address().port}`);
+});
